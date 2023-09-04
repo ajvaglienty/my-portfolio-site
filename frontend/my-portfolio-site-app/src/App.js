09a88316cfb00f8c229514114c5ea7d511c7ed3c
Andrew Vaglienty <ajvaglienty@gmail.com>
2023-09-04 10:58:22 -0700

    Change output

--- frontend/my-portfolio-site-app/src/App.js
+++ frontend/my-portfolio-site-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello Azure.
+          Hello Azure, it's Andrew.
         </p>
       </header>
     </div>
